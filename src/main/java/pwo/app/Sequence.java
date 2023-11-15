ddb399eb8b228804be1cbf8f1f50f3797f565882
michalgalant00 <s95401@pollub.edu.pl>
2023-11-15 10:58:02 +0100

    Dystrybucja 1

--- src/main/java/pwo/app/Sequence.java
+++ src/main/java/pwo/app/Sequence.java
@@ -4,6 +4,16 @@ public class Sequence {
 
     public static void main(String[] args) {
 
-        (new SeqToFileApp()).run(args);
+        switch (args.length) {
+            case 3:
+                (new SeqToOutApp()).run(args);
+                break;
+            case 4:
+                (new SeqToFileApp()).run(args);
+                break;
+            default:
+                System.out.println("!Illegal arguments\n"
+                        + "Legal usage: seqName from to [fileName]");
+        }
     }
 }
